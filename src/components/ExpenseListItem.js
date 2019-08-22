0bea3c31f1ed9e145525a3788886fa8019230687
waellaataoui <waellaataoui@rocketmail.com>
2019-08-22 15:06:55 +0100

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 const ExpenseListItem = (props) => {
   return (
     <div>
@@ -7,7 +9,8 @@ const ExpenseListItem = (props) => {
         <h3>{props.expense.description}</h3>
       </Link>
       <p>
-        {props.expense.amount}-{props.expense.createdAt}
+        {numeral(props.expense.amount / 100).format("$0,0.00")}-
+        {moment(props.expense.createdAt).format("MMMM Do,YYYY")}
       </p>
     </div>
   );
